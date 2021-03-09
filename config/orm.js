1e171b6f85aca2a100666217ae6c7d547a4c891e
jacob-af <jfeitler@gmail.com>
2021-03-08 23:10:04 -0500

    changed how callbacks are handled

--- config/orm.js
+++ config/orm.js
@@ -2,20 +2,29 @@ let connection = require("./connection");
 
 const orm = {
   selectAll(tableName, callback) {
-    connection.query("Select * FROM ??", tableName, (res) => callback(res));
+    connection.query("Select * FROM ??", tableName, (err, res) => {
+      if (err) throw err;
+      callback(res);
+    });
   },
   insertOne(tableName, columnName, value, callback) {
     connection.query(
       "INSERT INTO ?? (??) VALUES ?",
       [tableName, columnName, value],
-      (res) => callback(res)
+      (err, res) => {
+        if (err) throw err;
+        callback(res);
+      }
     );
   },
   updateOne(tableName, columnValObject, condition, callback) {
     connection.query(
       "UPDATE ?? SET ?? WHERE ?",
-      [tableName, columnValObj, condition],
-      (res) => callback(res)
+      [tableName, columnValObject, condition],
+      (err, res) => {
+        if (err) throw err;
+        callback(res);
+      }
     );
   },
 };
