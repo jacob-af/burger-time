49fcc74271dbfaffc098cbdf5a4391ba4faf86e8
jacob-af <jfeitler@gmail.com>
2021-03-09 12:15:33 -0500

    add burger functionality built

---
+++ public/assets/js/burger.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (event) {
+    console.info("DOM loaded");
+  }
+
+  const devourBurgerButton = document.querySelectorAll("devour-burger");
+
+  const addBurger = document.getElementById("burger-form");
+
+  if (addBurger) {
+    addBurger.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      // Grabs the value of the textarea that goes by the name, "quote"
+      const newBurger = {
+        burger_name: document.getElementById("add_burger").value.trim(),
+      };
+
+      // Send POST request to create a new quote
+      fetch("/api/cats", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+
+        // make sure to serialize the JSON body
+        body: JSON.stringify(newBurger),
+      }).then(() => {
+        // Empty the form
+        document.getElementById("add_burger").value = "";
+
+        // Reload the page so the user can see the new quote
+        console.log("Created a new cat!");
+        location.reload();
+      });
+    });
+  }
+});
