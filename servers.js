222412455207b3fccbe0e05a854e020a89222547
jacob-af <jfeitler@gmail.com>
2021-03-09 10:40:45 -0500

    fixed syntax error in requiring routes

---
+++ servers.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
