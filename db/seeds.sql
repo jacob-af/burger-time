05519c45cd4153ebc4e10e9e0436bebd341d7329
jacob-af <jfeitler@gmail.com>
2021-03-08 22:28:49 -0500

    schema and seed sqls created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+Values ("Juicy Lucy"),
+    ("Rodeo Clown Catastrope"),
+    ("Cheese with Cheese"),
+    ("Deviled Egg Dissapointment"),
+    ("Heart Attack Hotel");
